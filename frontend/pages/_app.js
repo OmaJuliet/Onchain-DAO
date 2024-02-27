fbb69b7c054da7f8fb9e000c01552f0ce8a1b300
Juliet Oma <julieto2001@gmail.com>
2024-02-27 18:41:46 +0100

    first commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,34 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import "@/styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "CryptoDevs DAO",
+  projectId: "1ec1202a0a3d78e409f8c4c269ce72e4",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+
+// projectId '1ec1202a0a3d78e409f8c4c269ce72e4' gotten from WalletConnect after signing up and creating a project
